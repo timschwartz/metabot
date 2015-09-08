6c4b603fb45754dc0d048a21e2ba72ecb9ac4139
Tim Schwartz <tim@metaverse.systems>
2015-09-07 19:54:34 -0500

    Adds skeleton main() and sets up autotools and Doxygen

---
+++ include/metabot.h
@@ -0,0 +1,10 @@
+#ifndef METABOT_H
+#define METABOT_H
+class Metabot
+{
+  public:
+    Metabot();
+
+    std::string name;
+};
+#endif
---
+++ src/metabot.cpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <metabot.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
